ee5286715d8047bcbfe46fe8bdabec6e9651c5da
Rafael Mendoza <rafael.mendoza64@gmail.com>
2023-01-03 16:32:56 -0500

    implement 2d ui elements

--- Assets/Scripts/Interface/Components/PlayerController.cs
+++ Assets/Scripts/Interface/Components/PlayerController.cs
@@ -1,9 +1,52 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
+    public SpriteRenderer playerSprite;
+    public List<Sprite> idleSprites;
+    public List<Sprite> runningSprites;
+    public bool running;
     // Movement speed of the player
     public float speed = 5f;
+
+    void Start()
+    {
+        StartCoroutine("Animate");
+    }
+
+    IEnumerator Animate()
+    {
+        while(true)
+        {
+            if(running)
+            {
+                foreach (Sprite frame in runningSprites)
+                {
+                    playerSprite.sprite = frame;
+                    yield return new WaitForSeconds(0.15f);
+                }
+    
+            } else
+            {
+                
+                foreach (Sprite frame in idleSprites)
+                {
+                    playerSprite.sprite = frame;
+                    yield return new WaitForSeconds(0.25f);
+                }
+                idleSprites.Reverse();
+                foreach (Sprite frame in idleSprites)
+                {
+                    playerSprite.sprite = frame;
+                    yield return new WaitForSeconds(0.25f);
+                }
+                idleSprites.Reverse();
+            }
+            
+        }
+    }
 
     void Update()
     {
@@ -16,5 +59,6 @@ public class PlayerController : MonoBehaviour
         Vector3 horizontal = Vector3.right * horizontalInput * speed * Time.deltaTime;
 
         transform.position = transform.position + vertical + horizontal;
+
     }
 }
